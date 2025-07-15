ad45a0bcc9bc2d3f0220c22f0d6e359ba07919a1
Nomen Conservandum <hramov.sanya2018@yandex.ru>
2025-07-15 23:50:59 +0400

    implement basic startup authentication, API key insertion page

--- scripts/auth-dialog.js
+++ scripts/auth-dialog.js
@@ -1,10 +1,69 @@
-const BaseURL = localStorage.getItem('BaseURL');
+// Initial check
+BaseURL = "";
 
-if(localStorage.getItem('JWT-ACCESS-TOKEN')) { // temporary solution
-    // main page
-} else {
-    signUpDialog('signInDialog');
+authorization()
+
+async function authorization() {
+    if(localStorage.getItem('BaseURL')) {
+        // checks the URL
+        BaseURL = localStorage.getItem('BaseURL');
+        try {
+            const response = await fetch(BaseURL + 'check', { // TODO: check should also require token
+                headers: {'Content-Type': 'application/json'},
+                method: 'GET'
+            });
+            const data = await response.json();
+
+            if (!response.ok) {
+                insertAPIKeyDialog();
+                localStorage.setItem('BaseURL', "");
+                alert("The API key was invalid");
+            } else {
+                signInDialog('signUpDialog'); // temporary solution. Will be fixed with JWT
+            }
+            
+        } catch (error) {
+            insertAPIKeyDialog();
+            localStorage.setItem('BaseURL', "");
+            alert("The API key was invalid");
+        }
+    } else {
+        insertAPIKeyDialog();
+        localStorage.setItem('BaseURL', "");
+    }
 }
+
+async function APIKeyInsertion() {
+    key = document.getElementById('api-key').value;
+    localStorage.setItem('BaseURL', key);
+    BaseURL = key;
+    document.getElementById('insertAPIKey').style.display = 'none';
+    // a server check
+    try {
+        const response = await fetch(BaseURL + 'check', { // TODO: check should also require token
+            headers: {'Content-Type': 'application/json'},
+            method: 'GET'
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+            localStorage.setItem('BaseURL', "");
+            alert("The API key is invalid");
+        } else {
+            signUpDialog('insertAPIKey');
+        }
+        
+    } catch (error) {
+        alert("The API key is invalid");
+    }
+}
+
+
+function insertAPIKeyDialog() {
+    document.title = 'Authorization'
+    document.getElementById('insertAPIKey').style.display = 'block';
+}
+
 
 function signUpDialog(name) {
     document.getElementById(name).style.display = 'none';
